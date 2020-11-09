2bca4d9f87895ba0eca700a303b10c0daade997d
Schnittchen1423 <74188811+Schnittchen1423@users.noreply.github.com>
2020-11-09 14:06:31 +0100

    Here is a countdown.

---
+++ countdown.py
@@ -0,0 +1,45 @@
+import os
+import time
+import socket
+
+#This is a countdown. Or call it timer. What ever. You just have to run this code and it'll ask you what amount of seconds it should run.
+#Savity warning: Only works on Mac (Apple). 
+
+#preparation
+os.system("clear")
+print("______________________  _____________________  ")
+print("___  __/____  _/___   |/  /___  ____/___  __ \ ")
+print("__  /    __  /  __  /|_/ / __  __/   __  /_/ / ")
+print("_  /    __/ /   _  /  / /  _  /___   _  _, _/  ")
+print("/_/     /___/   /_/  /_/   /_____/   /_/ |_|   ")
+
+print("Credits to Schnitchen1423:")
+print("YouTube: https://www.youtube.com/channel/UCkZxbU5wz6hNQM771SlL12w ")
+print("GitHub: https://github.com/Schnittchen1423 ")
+
+# String
+CountDown = raw_input("Seconds: ")
+
+# Integer
+int_CountDown = int(CountDown)
+expired = False
+
+
+while expired == False:
+	try:
+		print(int_CountDown)
+		time.sleep(1)
+		int_CountDown = int_CountDown - 1
+		
+		if int_CountDown == 0:
+		 print("Countdown expired")
+		 expired = True
+		 print("Stopped")
+		 time.sleep(3)
+		 os.system("clear")
+		 print("Countdown expired.")
+		 print("Program stopped.")
+		 os.system("exit")
+
+	except:
+		print("Error")
